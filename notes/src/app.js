a4bc4b30cf28cbcfc204777c612cc6e4e574a8cf
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2024-04-10 23:00:14 +0800

    Add files via upload

---
+++ notes/src/app.js
@@ -0,0 +1,8 @@
+import "./styles/style.css";
+
+import "./script/components/main.js";
+import home from "./script/view/home.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  home();
+});
---
+++ notes/src/script/components/main.js
@@ -0,0 +1,12 @@
+import "./app-bar.js";
+import "./footer-bar.js";
+
+import "./note-list.js";
+import "./note-item.js";
+
+
+
+import "./form-input.js";
+
+import "./archived-list.js";
+import "./archived-item.js";
